000d92d9a247b59d425bcb87f0755177c167c526
Tim Kelty <tim@fusionary.com>
2017-12-18 19:14:34 -0500

    Align options with node api

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -4,21 +4,15 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 module.exports = (neutrino, opts = {}) => {
   const options = merge({
     pluginId: 'stylelint',
-    plugin: {
-      files: '**/*.+(css|scss|sass|less)',
-      context: neutrino.options.source,
-      failOnError: neutrino.options.command !== 'start',
-      quiet: neutrino.options.command === 'start'
-    }
+    files: '**/*.+(css|scss|sass|less)',
+    context: neutrino.options.source,
+    failOnError: neutrino.options.command !== 'start',
+    quiet: neutrino.options.command === 'start'
   }, opts);
 
-  if (options.stylelint) {
-    options.plugin = merge(options.plugin, {
-      config: options.stylelint
-    })
-  }
-
-  const getStylelintRcConfig = config => config.plugin(options.pluginId).get('args')[0].config;
+  const getStylelintRcConfig = config => config
+    .plugin(options.pluginId)
+    .get('args')[0].config || {};
 
   neutrino.register(
     'stylelintrc',
@@ -28,5 +22,5 @@ module.exports = (neutrino, opts = {}) => {
 
   neutrino.config
     .plugin(options.pluginId)
-    .use(StylelintPlugin, [options.plugin]);
+    .use(StylelintPlugin, [options]);
 };
