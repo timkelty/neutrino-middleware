e0380a3be2695b50ca95fba550572c754b33b4b3
Tim Kelty <tim@fusionary.com>
2018-01-04 08:06:37 -0500

    Add web html

--- packages/critical-css/index.js
+++ packages/critical-css/index.js
@@ -1,5 +1,5 @@
 const merge = require('deepmerge');
-const { omit } = require('ramda');
+const {omit} = require('ramda');
 const HtmlCriticalPlugin = require('html-critical-webpack-plugin');
 
 module.exports = (neutrino, opts = {}) => {
@@ -8,6 +8,24 @@ module.exports = (neutrino, opts = {}) => {
     base: neutrino.options.output
   }, opts);
 
+  // Add html targets from @neutrinojs/web
+  if (!options.src && !options.html && !options.targets) {
+    Object.assign(options, {
+      targets: Object.keys(neutrino.config.plugins.entries())
+        .filter(pluginId => pluginId.startsWith('html-'))
+        .map(pluginId => {
+          const filename = neutrino.config.plugin(pluginId).get('args')[0].filename;
+
+          return {
+            inline: true,
+            src: filename,
+            dest: filename,
+            pluginId: `${options.pluginId}-${pluginId}`
+          };
+        })
+    });
+  }
+
   const targets = (options.targets || [options])
     .map((target, index) => {
       if (!target.pluginId && index > 0) {
@@ -15,14 +33,13 @@ module.exports = (neutrino, opts = {}) => {
           pluginId: `${options.pluginId}-${index}`
         });
       }
-      console.log(options);
+
       return merge(
         omit('targets', options),
         target
       );
     });
 
-  // console.log(targets);
   targets.forEach((target, index) => {
     neutrino.config
       .plugin(target.pluginId)
